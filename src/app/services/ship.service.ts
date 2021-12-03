54fc77d14b21ed4e6401395ce67255104dc8390e
bojan3 <bojanskokicxd@outlook.com>
2021-12-04 00:15:17 +0100

    ship list

---
+++ src/app/services/ship.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Ship } from '../entity/Ship';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShipService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllShips(): Observable<Ship[]>{
+    return this.http.get<Ship[]>('http://localhost:8081/api/ship/all')
+  }
+}
