677b75e2f12797534998d6fbe97104071d2fe098
bojan3 <bojanskokicxd@outlook.com>
2021-12-04 23:31:24 +0100

    adventures fix

--- src/app/entity/Ship.ts
+++ src/app/entity/Ship.ts
@@ -5,26 +5,26 @@ export class Ship{
   name: string;
   length: number;
   description: string;
-  average_mark: number;
-  rent_price: number;
-  engine_number: number;
-  engine_power: number;
-  max_speed: number;
+  averageMark: number;
+  rentPrice: number;
+  engineNumber: number;
+  enginePower: number;
+  maxSpeed: number;
   capacity: number;
-  cancel_rate: number;
+  cancelRate: number;
 
-  constructor(id: number, name: string, length: number, description: string, average_mark: number, rent_price: number, engine_number: number, engine_power: number, max_speed: number, capacity: number, cancel_rate: number){
+  constructor(id: number, name: string, length: number, description: string, averageMark: number, rentPrice: number, engineNumber: number, enginePower: number, maxSpeed: number, capacity: number, cancelRate: number){
     this.id = id;
     this.name = name;
     this.length = length;
     this.description = description;
-    this.average_mark = average_mark;
-    this.rent_price = rent_price;
-    this.engine_number = engine_number;
-    this.engine_power = engine_power;
-    this.max_speed = max_speed;
+    this.averageMark = averageMark;
+    this.rentPrice = rentPrice;
+    this.engineNumber = engineNumber;
+    this.enginePower = enginePower;
+    this.maxSpeed = maxSpeed;
     this.capacity = capacity;
-    this.cancel_rate = cancel_rate;
+    this.cancelRate = cancelRate;
   }
 
 }
