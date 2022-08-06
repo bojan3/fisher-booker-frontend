f66d3c330f8bbd577f2c685150f1299b45258c91
bojan3 <bojanskokicxd@outlook.com>
2022-08-06 11:11:54 +0200

    cottage sort system

--- src/app/components/cottages/cottages.component.ts
+++ src/app/components/cottages/cottages.component.ts
@@ -3,6 +3,7 @@ import { CottageDTO } from '../../entity/DTO/CottageDTO';
 import { AccountService } from 'src/app/services/account.service';
 import { ClientService } from 'src/app/services/client.service';
 import { CottageService } from 'src/app/services/cottage.service';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cottages',
@@ -17,13 +18,26 @@ export class CottagesComponent implements OnInit {
   forClientSubscriptions: boolean = false;
   dateInput!: Date;
   cottages: CottageDTO[] = [];
+  sortByGroup!: FormGroup;
+  orderGroup!: FormGroup;
 
   constructor(private cottageService: CottageService,
               private clientService: ClientService,
-              private accountService: AccountService) { }
+              private accountService: AccountService,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    // this.buildForm();
+
+    this.sortByGroup = new FormGroup({
+      'sortByRadio' : new FormControl()
+    });
+
+    this.orderGroup = new FormGroup({
+      'orderRadio' : new FormControl()
+    });
+
+    this.orderGroup.patchValue({orderRadio: 'ASC'});
+
     if (this.forCottageOwner) {
       this.cottageService.getAllCottagesByOwner().subscribe((cottages) => (this.cottages = cottages));
     } if(this.forClientSubscriptions){
@@ -32,23 +46,10 @@ export class CottagesComponent implements OnInit {
     else {
       this.cottageService.getAllCottages().subscribe((cottages) => (this.cottages = cottages));
     }
-    this.cottageService.getAllCottagesByName().subscribe((cottages) => (this.cottages = cottages));
-
   }
 
-  getSorted(e: any){
-   console.log(e.target.value)
-  }
-
-  sortByName() {
-    this.cottageService.getAllCottagesByName().subscribe((cottages) => (this.cottages = cottages));
-  }
-
-  sortByPrice() {
-    this.cottageService.getAllCottagesByPrice().subscribe((cottages) => (this.cottages = cottages));
-  }
-  sortByRating() {
-    this.cottageService.getAllCottagesByRating().subscribe((cottages) => (this.cottages = cottages));
+  getSorted(){
+   this.cottageService.getAllCottages(this.sortByGroup.value.sortByRadio, this.orderGroup.value.orderRadio).subscribe((cottages) => (this.cottages = cottages));
   }
 
   notClientSubscriptions(): boolean {
@@ -56,12 +57,7 @@ export class CottagesComponent implements OnInit {
   }
 
   onSearch(){
-    console.log(this.dateInput);
     this.cottageService.getByDate(this.dateInput).subscribe((cottages) => (this.cottages = cottages));
   }
-  // buildForm() {
-  //   this.form = this.formBuilder.group({
-  //   date: [this.dateInput]
-  //   })
-  // }
+
 }
